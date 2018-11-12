b55d68ffc1f42aa7e60584b962b34f35db1bd1df
zhongdd <841370939@qq.com>
2018-11-12 15:09:26 +0800

    20181112提交

--- testcase/testLogin.py
+++ testcase/testLogin.py
@@ -1,12 +1,14 @@
-from selenium import webdriver
+from webDriver import driver
+from base.baseload import BosmaSmart
+from time import sleep
 import unittest
-import time
+
 
 class login(unittest.TestSuite):
     '''登录模块'''
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get("https://rd.bosma.cn/uc/#/welcome")
+        self.driver = driver.get_driver(self)
+        self.driver.get('https://rd.bosma.cn/uc/#/welcome')
     def test_admin_login(self):
         '''正确流程的登录操作'''
         email = self.driver.find_element_by_xpath("//form[1]//input[@type='email']")
@@ -15,7 +17,7 @@ class login(unittest.TestSuite):
         email.send_keys("1451953028@qq.com")
         password.send_keys("zdd123456")
         button.click()
-        time.sleep(2)
+        sleep(2)
     def test_login_emailempty(self):
         '''登录邮箱未空'''
         email = self.driver.find_element_by_xpath("//form[1]//input[@type='email']")
@@ -24,7 +26,7 @@ class login(unittest.TestSuite):
         email.send_keys("")
         password.send_keys("zdd123456")
         button.click()
-        time.sleep(2)
+        sleep(2)
     def test_login_wrongpassword(self):
         '''登录密码错误'''
         email = self.driver.find_element_by_xpath("//form[1]//input[@type='email']")
@@ -33,4 +35,7 @@ class login(unittest.TestSuite):
         email.send_keys("1451953028@qq.com")
         password.send_keys("12345665fgdf")
         button.click()
-        time.sleep(2)
+        sleep(2)
+    def tearDown(self):
+        self.driver.quit()
+        sleep(3)

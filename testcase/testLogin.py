3e7631c6cdad3a828066eeca8eb3b6adec69bd31
zhongdd <841370939@qq.com>
2018-11-12 09:07:22 +0800

    20181108提交

--- testcase/testLogin.py
+++ testcase/testLogin.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 import unittest
-
+import time
 
 class login(unittest.TestSuite):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.get("https://rd.bosma.cn/uc/#/welcome")
     def test_admin_login(self):
-        email = self.driver.find_element_by_xpath("")
-        password = self.driver.find_element_by_xpath("")
-        button = self.driver.find_element_by_xpath("")
+        email = self.driver.find_element_by_xpath("//form[1]//input[@type='email']")
+        password = self.driver.find_element_by_xpath("//form[1]//input[@type='password']")
+        button = self.driver.find_element_by_xpath("//form[1]//button")
+        email.send_keys("1451953028@qq.com")
+        password.send_keys("zdd123456")
+        button.click()
+        time.sleep(2)
